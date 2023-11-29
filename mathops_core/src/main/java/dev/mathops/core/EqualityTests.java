7f13e74d923caf8844a81110677bce9fd210423b
Steve Benoit <benoit@math.colostate.edu>
2023-11-29 16:46:00 -0700

    Import counts by major report.

--- mathops_core/src/main/java/dev/mathops/core/EqualityTests.java
+++ mathops_core/src/main/java/dev/mathops/core/EqualityTests.java
@@ -1,5 +1,11 @@
 package dev.mathops.core;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Equality tests that supplement those provided by Java.
  */
@@ -26,5 +32,30 @@ public enum EqualityTests {
     public static int objectHashCode(final Object obj) {
 
         return obj == null ? 0 : obj.hashCode();
+    }
+
+    /**
+     * Sorts a map by value.
+     *
+     * @param map the map to sort
+     * @param descending true to sort descending
+     * @return the sorted map
+     * @param <K> the key type
+     * @param <V> the value type
+     */
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(final Map<K, V> map,
+                                                                             final boolean descending) {
+        final List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        if (descending) {
+            Collections.reverse(list);
+        }
+
+        final Map<K, V> result = new LinkedHashMap<>(list.size());
+        for (final Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
     }
 }
