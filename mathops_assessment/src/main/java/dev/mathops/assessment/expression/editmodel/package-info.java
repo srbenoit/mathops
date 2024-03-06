ed119d3e3501c93fce7c17c5e12883b0274f44bd
benoit <steve.benoit@colostate.edu>
2024-03-05 19:04:36 -0700

    Work on expression model.

--- mathops_assessment/src/main/java/dev/mathops/assessment/expression/editmodel/package-info.java
+++ mathops_assessment/src/main/java/dev/mathops/assessment/expression/editmodel/package-info.java
@@ -2,14 +2,34 @@
  * Classes that represent the "model" of an expression within the editor.
  *
  * <p>
- * Every object that can flow in a linear sequence in an expression is a "glyph".  This includes constructions like
- * fractions, radicals, vectors and matrices, strings, etc.  A sequence of these glyphs are arranged in a line, and
- * a cursor can navigate along that line.
+ * A top-level "expression" model object is a container for a sequence of expression objects:
+ * <ul>
+ *     <li>Operators</li>
+ *     <li>Decimal digits, decimal points, and the Engineering "E" symbol</li>
+ *     <li>Symbolic constants like PI, E, and I</li>
+ *     <li>Boolean constants</li>
+ *     <li>String constants</li>
+ *     <li>Variable names</li>
+ *     <li>Fractions (with expressions as numerator and denominator)</li>
+ *     <li>Base/Exponent constructions (with expressions as base and exponent)</li>
+ *     <li>Radical constructions (with an expression under a radical)</li>
+ *     <li>N-th Root Constructions (with an integer root and an expression under a radical)</li>
+ *     <li>Parenthesized expressions</li>
+ *     <li>Vectors with expression components</li>
+ *     <li>Matrices with expression entries</li>
+ *     <li>Function invocations, with one or more expressions as arguments</li>
+ * </ul>
  *
  * <p>
- * When a glyph may contain children, the cursor can "step into" the glyph from either end, and can then "step out of"
- * and back to its container's sequence of cursor positions.  The cursor object has an "in sub-expression" flag that
- * indicates it is within a sub-expression of the child to the right of the cursor.  When this is true, the cursor
- * position tracked by the sub-expression governs.
+ * Every object either represents a single "cursor step" in an expression, or is a compound object with sub-expressions,
+ * where one cursor step moves into the object, one cursor step moves from one child sub-expression to the next, and
+ * one cursor step moves out of the object.
+ *
+ * <p>
+ * Expression models support the following types of action:
+ * <ul>
+ *     <li>Insert one of the above things at a specified position</li>
+ *     <li>Delete the object at a specified position</li>
+ * </ul>
  */
 package dev.mathops.assessment.expression.editmodel;
