959a76f468ece5356bc1738f7d6507988beb9f87
Steve Benoit <benoit@math.colostate.edu>
2024-07-12 07:39:58 -0600

    Update testing app version

--- mathops_app/src/main/java/dev/mathops/app/deploy/TestingAppBuilder.java
+++ mathops_app/src/main/java/dev/mathops/app/deploy/TestingAppBuilder.java
@@ -29,7 +29,7 @@ import java.util.zip.ZipEntry;
 final class TestingAppBuilder {
 
     /** The version number. */
-    private static final String VERSION = "1.2.022";
+    private static final String VERSION = "1.2.023";
 
     /** Directory where project is stored. */
     private final File projectDir;
