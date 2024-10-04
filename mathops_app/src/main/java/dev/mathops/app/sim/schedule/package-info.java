a1ff711ed2970a01c8fc0fc0f7de13e82b15fad7
benoit <steve.benoit@colostate.edu>
2024-10-03 18:50:04 -0600

    Work on Spur Sim.

--- mathops_app/src/main/java/dev/mathops/app/sim/schedule/package-info.java
+++ mathops_app/src/main/java/dev/mathops/app/sim/schedule/package-info.java
@@ -4,14 +4,26 @@
  *
  * <p>
  * Each 1-credit unit requires 800 contact minutes (counting final exam week).  This requires 50 minutes per week for a
- * 16-week term, 70 per week for a 12-week term, 90 per week for a 9-week term, 100 per week for an 8-week term, 135 per
- * week for a 6-week term, 160 per week for a 5-week term, and 200 per week for a 4-week term (the shortest we
- * contemplate). Most courses are 3-credits, so daily schedules should be designed to accommodate 3-credit courses.
+ * 16-week term, 70 per week for a 12-week term, 100 per week for an 8-week term (the shortest we contemplate). Most
+ * courses are 3-credits, so daily schedules should be designed to accommodate 3-credit courses.
  *
  * <p>
  * In a 16-week semester, a 3-credit course (needing 150 minutes per week of contact) could meet for three days at 50
  * minutes per day or two days at 75 minutes per day, so we might choose to have Monday, Wednesday, and Friday operate
- * on 1-hour blocks with 10-minute passing time, and Tuesday, Thursday, and Saturday/Sunday, if they are included,
+ * on 1-hour periods with 10-minute passing time, and Tuesday, Thursday, and Saturday/Sunday, if they are included,
  * operate with 75-minute periods with 15-minute passing time.
+ *
+ * <p>
+ * In a 12-week semester, a 3-credit course (needing 210 minutes per week of contact) could meet for three days at 70
+ * minutes per day or two days at 105 minutes per day, so we might choose to have Monday, Wednesday, and Friday operate
+ * on 90-minute periods with 20-minute passing time, and Tuesday, Thursday, and Saturday/Sunday, if they are included,
+ * operate with 120-minute periods with 15-minute passing time.
+ *
+ * <p>
+ * In an 8-week semester, a 3-credit course (needing 300 minutes per week of contact) could meet for three days at 100
+ * minutes per day or two days at 150 minutes per day, so we might choose to have Monday, Wednesday, and Friday operate
+ * on 50-minute periods with 10-minute passing time (2 periods per class), and Tuesday, Thursday, and Saturday/Sunday,
+ * if they are included, operate with 75-minute periods with 15-minute passing time (2 periods per class).  This has the
+ * advantage that one could run a "late-start" 8-week semester in tandem with a 16-week semester.
  */
 package dev.mathops.app.sim.schedule;
