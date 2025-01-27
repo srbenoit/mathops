0bec343894e3ac99355dbf869631117ffbcafc4a
Steve Benoit <benoit@math.colostate.edu>
2025-01-27 09:09:19 -0700

    Removed course visualizer app.

--- mathops_app/src/main/java/dev/mathops/app/course/visualizer/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * A visualizer application that presents a course directory structure and allows the user to edit basic (text) files
- * and navigate to subdirectories in the system file explorer.
- */
-package dev.mathops.app.course.visualizer;
