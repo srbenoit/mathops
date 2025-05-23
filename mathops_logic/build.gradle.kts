d9d354c030a0c0deaaab6f22f8171a23c5d59276
Steve Benoit <steve.benoit@colostate.edu>
2025-05-23 10:27:02 -0600

    Update to build script

---
+++ mathops_logic/build.gradle.kts
@@ -0,0 +1,43 @@
+tasks.test {
+    useJUnitPlatform()
+}
+
+
+plugins {
+    id("java")
+}
+
+sourceSets {
+    main {
+        output.setResourcesDir(file("build/classes/java/main"))
+    }
+}
+
+group = "dev.mathops.logic"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":mathops_db"))
+    implementation(project(":mathops_assessment"))
+    implementation(files("../../mathops_commons/out/libs/mathops_commons.jar"))
+    implementation(files("../../mathops_text/out/libs/mathops_text.jar"))
+
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0-M2")
+    compileOnly("jakarta.websocket:jakarta.websocket-api:2.2.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+tasks {
+    withType<JavaCompile> {
+        options.compilerArgs.add("-Xlint:unchecked")
+    }
+}
