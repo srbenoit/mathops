6816c28987c1eb98a6dd3710f0ff6d27159b3d51
Steve Benoit <steve.benoit@colostate.edu>
2025-09-08 13:51:10 -0600

    Fix OPS Canvas senders.

--- mathops_app/src/main/java/dev/mathops/app/ops/snapin/messaging/tosend/CanvasMessageSenders.java
+++ mathops_app/src/main/java/dev/mathops/app/ops/snapin/messaging/tosend/CanvasMessageSenders.java
@@ -13,6 +13,9 @@ import java.util.Map;
  */
 public final class CanvasMessageSenders {
 
+    /** The host address for Canvas. */
+    private static final String CANVAS_HOST = "https://colostate.instructure.com";
+
     /** The mail senders, map from course to map from section to sender. */
     private final Map<String, Map<String, CanvasMessageSend>> senders;
 
@@ -43,56 +46,58 @@ public final class CanvasMessageSenders {
 
         final CanvasCourseIdMap courseIdMap = new CanvasCourseIdMap();
 
-        final CanvasMessageSend sender117ri = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
-                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M117, "002"));
-//        senders117.put("001", sender117ri);
+        // FIXME: Configured these correctly each Fall, Spring, and Summer term
+
+        final CanvasMessageSend sender117ri = new CanvasMessageSend(theCache, CANVAS_HOST,
+                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M117, "001"));
+        senders117.put("001", sender117ri);
         senders117.put("002", sender117ri);
 
-        final CanvasMessageSend sender117ce = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
+        final CanvasMessageSend sender117ce = new CanvasMessageSend(theCache, CANVAS_HOST,
                 accessToken, courseIdMap.getCanvasId(RawRecordConstants.M117, "801"));
         senders117.put("401", sender117ce);
         senders117.put("801", sender117ce);
 //        senders117.put("809", sender117ce);
 
-//        final CanvasMessageSend sender118ri = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
-//                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M118, "001"));
-//        senders118.put("001", sender118ri);
-//        senders118.put("002", sender118ri);
+        final CanvasMessageSend sender118ri = new CanvasMessageSend(theCache, CANVAS_HOST,
+                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M118, "001"));
+        senders118.put("001", sender118ri);
+        senders118.put("002", sender118ri);
 
-        final CanvasMessageSend sender118ce = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
+        final CanvasMessageSend sender118ce = new CanvasMessageSend(theCache, CANVAS_HOST,
                 accessToken, courseIdMap.getCanvasId(RawRecordConstants.M118, "801"));
         senders118.put("401", sender118ce);
         senders118.put("801", sender118ce);
 //        senders118.put("809", sender118ce);
 
-//        final CanvasMessageSend sender124ri = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
-//                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M124, "001"));
-//        senders124.put("001", sender124ri);
-//        senders124.put("002", sender124ri);
+        final CanvasMessageSend sender124ri = new CanvasMessageSend(theCache, CANVAS_HOST,
+                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M124, "001"));
+        senders124.put("001", sender124ri);
+        senders124.put("002", sender124ri);
 
-        final CanvasMessageSend sender124ce = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
+        final CanvasMessageSend sender124ce = new CanvasMessageSend(theCache, CANVAS_HOST,
                 accessToken, courseIdMap.getCanvasId(RawRecordConstants.M124, "801"));
         senders124.put("401", sender124ce);
         senders124.put("801", sender124ce);
 //        senders124.put("809", sender124ce);
 
-//        final CanvasMessageSend sender125ri = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
-//                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M125, "001"));
-//        senders125.put("001", sender125ri);
-//        senders125.put("002", sender125ri);
+        final CanvasMessageSend sender125ri = new CanvasMessageSend(theCache, CANVAS_HOST,
+                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M125, "001"));
+        senders125.put("001", sender125ri);
+        senders125.put("002", sender125ri);
 
-        final CanvasMessageSend sender125ce = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
+        final CanvasMessageSend sender125ce = new CanvasMessageSend(theCache, CANVAS_HOST,
                 accessToken, courseIdMap.getCanvasId(RawRecordConstants.M125, "801"));
         senders125.put("401", sender125ce);
         senders125.put("801", sender125ce);
 //        senders125.put("809", sender125ce);
 
-//        final CanvasMessageSend sender126ri = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
-//                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M126, "001"));
-//        senders126.put("001", sender126ri);
-//        senders126.put("002", sender126ri);
+        final CanvasMessageSend sender126ri = new CanvasMessageSend(theCache, CANVAS_HOST,
+                accessToken, courseIdMap.getCanvasId(RawRecordConstants.M126, "001"));
+        senders126.put("001", sender126ri);
+        senders126.put("002", sender126ri);
 
-        final CanvasMessageSend sender126ce = new CanvasMessageSend(theCache, "https://colostate.instructure.com",
+        final CanvasMessageSend sender126ce = new CanvasMessageSend(theCache, CANVAS_HOST,
                 accessToken, courseIdMap.getCanvasId(RawRecordConstants.M126, "801"));
         senders126.put("401", sender126ce);
         senders126.put("801", sender126ce);
